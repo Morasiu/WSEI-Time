9873f5f74a4b80e0e0c6873d1c20d64b26e75979
Morasiu <morasiu2@gmail.com>
2019-01-18 10:02:29 +0100

    ClassLib init

---
+++ TimeClassLib/Time.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TimeClassLib {
+    public struct Time {
+        public byte Hours => _hours;
+        public byte Minutes => _minutes;
+        public byte Seconds => _seconds;
+
+        public Time(byte hours, byte minutes, byte seconds) {
+            _hours = hours;
+            _minutes = minutes;
+            _seconds = seconds;
+        }
+
+        private readonly byte _hours;
+        private readonly byte _minutes;
+        private readonly byte _seconds;
+    }
+}
